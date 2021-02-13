0fa041aaa6b288710d224b294b6ab433e701c802
mrjoker85 <77496459+mrjoker85@users.noreply.github.com>
2021-02-13 10:59:52 +0330

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+
+//#consts
+const Discord = require(`discord.js`)
+const client = new Discord.Client();
+const prefix = ``
+const fs = require(`fs`);
+const memberCounter = require(`./events/member-counter`)
+module.exports = client;
+//#Handlers
+client.commands = new Discord.Collection();
+client.aliases = new Discord.Collection();
+client.categories = fs.readdirSync("./commands/");
+["command"].forEach(handler => {
+    require(`./handlers/${handler}`)(client);
+}); 
+//#Commands
+client.on('message', async message =>{
+    if(message.author.bot) return;
+    if(!message.content.startsWith(prefix)) return;
+    if(!message.guild) return;
+    if(!message.member) message.member = await message.guild.fetchMember(message);
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const cmd = args.shift().toLowerCase();
+    if(cmd.length == 0 ) return;
+    let command = client.commands.get(cmd)
+    if(!command) command = client.commands.get(client.aliases.get(cmd));
+    if(command) command.run(client, message, args) 
+})
+//Logging in to discord
+client.login(`Nzk0MTM0ODY3MDg3OTgyNjAy.X-2Zug.v1XCql73D1Nkb_3eRTl9IObVdgY`);

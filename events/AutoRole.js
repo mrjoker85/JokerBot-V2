8314244908887a365a2d4d087f55a3b27d2a2962
mrjoker85 <77496459+mrjoker85@users.noreply.github.com>
2021-02-14 14:11:48 +0330

    Update AutoRole.js

--- events/AutoRole.js
+++ events/AutoRole.js
@@ -2,7 +2,7 @@ const client = require(`../index`)
 // const Discord = require(`discord.js`)
 
 client.on(`guildMemberAdd`, guildMember =>{
-    let welcomeRole = guildMember.guild.roles.cache.find(role => role.name === `『❤️』Joker Shop Member`);
+    let welcomeRole = guildMember.guild.roles.cache.find(role => role.name === `『👤』 𝐌𝐞𝐦𝐛𝐞𝐫`);
 
     guildMember.roles.add(welcomeRole);
 })

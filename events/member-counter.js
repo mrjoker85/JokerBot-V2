0cb5c963b357496efa18023efec9eef88239d07c
mrjoker85 <77496459+mrjoker85@users.noreply.github.com>
2021-02-14 13:27:25 +0330

    Update member-counter.js

--- events/member-counter.js
+++ events/member-counter.js
@@ -2,7 +2,7 @@ module.exports = async (client) =>{
     const guild = client.guilds.cache.get(`781845235931152405`)
     setInterval(() =>{
         const memberCount = guild.memberCount;
-        const channel = guild.channels.cache.get(`794284079129296906`)
-        channel.setName(`【👥】𝐌𝐞𝐦𝐛𝐞𝐫𝐬 : ${memberCount.toLocaleString()}`)
+        const channel = guild.channels.cache.get(`810448550394855455`)
+        channel.setName(`⌠👥⌡𝐔𝐬𝐞𝐫𝐬 :${memberCount.toLocaleString()}`)
     }, 5000)
 }

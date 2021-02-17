bcc944fcfaab0f257586685fd75f46ffd6b8ecd4
Mr Joker <77496459+mrjoker85@users.noreply.github.com>
2021-02-17 12:41:04 +0330

    Update addrole.js

--- commands/mod/addrole.js
+++ commands/mod/addrole.js
@@ -10,8 +10,7 @@ module.exports = {
         //so firstly we will check whether the author of the message has permissions
         //this line means if the author doesn't have manage roles permission it will stop the process and send the following text
         if(!message.member.hasPermission("MANAGE_ROLES")) return message.channel.send('You do not have permission.')
-        if(!hasPermission("MANAGE_ROLES")) return message.channel.send('I do not have permission.')
-        //next we define some variables
+        if (!permissions.has("MANAGE_ROLES")) return message.channel.send('I do not have permission.')
         const target = message.mentions.members.first() //member = mentions
         if(!target) return message.channel.send('No member specified') //when no member is pinged
         const role = message.mentions.roles.first() // roles = mentions
